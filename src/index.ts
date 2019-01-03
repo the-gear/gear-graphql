577d5af88202cd69a1e6155c0e4cfe42b21c51a2
Pavel Lang <langpavel@phpskelet.org>
2019-01-03 15:48:06 +0100

    chore(package): initial commit
    Based on @the-gear/ts-lib-starter at v6.2.0-18-gd19d9f0
    https://github.com/the-gear/ts-lib-starter/tree/d19d9f0e18fe9271594992be0ad4b651ff3c7668

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Type definitions for @the-gear/graphql-gear 0.0.0
+ * Project: https://github.com/the-gear/graphql-gear
+ * Definitions by: Pavel Lang <https://github.com/the-gear>
+ * Definitions: https://github.com/the-gear/graphql-gear
+ * TypeScript Version: 3.2
+ */
+
+export { Greeter } from './greeter';
