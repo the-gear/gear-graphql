4280bf8620b0fc25cd90a65cdf558c798ee1b08d
Pavel Lang <langpavel@phpskelet.org>
2019-01-23 23:54:27 +0100

    chore(package): upgrade dependencies

--- src/json-stringify.ts
+++ src/json-stringify.ts
@@ -1,7 +1,7 @@
 import prettier from 'prettier';
 
 // tslint:disable-next-line:no-any
-export default function jsonStringify(obj: any, replacer?: ((key: string, value: any) => any)) {
+export default function jsonStringify(obj: any, replacer?: (key: string, value: any) => any) {
   return prettier.format(JSON.stringify(obj, replacer), {
     parser: 'json',
   });
